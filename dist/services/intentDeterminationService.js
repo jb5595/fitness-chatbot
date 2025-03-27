a41d69927f335bb39954368138dff114656465aa
Jacob Berman <jacobberman1995@gmail.com>
2025-03-26 20:04:06 -0600

    remove compiled code from git

--- dist/services/intentDeterminationService.js
+++
@@ -1,9 +0,0 @@
-import { OpenAIChatService } from "./openAIChatService.js";
-export class IntentDeterminationService {
-    static async determineIntent(customerMessage) {
-        const chatService = new OpenAIChatService();
-        const response = await chatService.chat([this.INTENT_PROMPT], [customerMessage]);
-        return response?.trim();
-    }
-}
-IntentDeterminationService.INTENT_PROMPT = `Classify the intent of this message as one of: "booking", "general question", "workout suggestion", or "other". Return only the intent in lowercase.`;
