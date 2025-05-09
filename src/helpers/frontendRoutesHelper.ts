9d72fdf7c891a01bfcdcfa1e623471750390f558
Jacob Berman <jacobberman1995@gmail.com>
2025-05-08 20:46:28 -0600

    pull frontend url from env file

--- src/helpers/frontendRoutesHelper.ts
+++ src/helpers/frontendRoutesHelper.ts
@@ -1,4 +1,8 @@
-const FRONT_END_BASE_URL = "https://fitness-chatbot-ui-git-main-jb5595s-projects.vercel.app";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const FRONT_END_BASE_URL = process.env.FRONT_END_BASE_URL;
 
 export const gymClientChatHistoryUrl = (gymPhoneNumber: string, clientPhoneNumber: string) => {
     return `${FRONT_END_BASE_URL}/chat-history/${gymPhoneNumber}?clientPhoneNumber=${clientPhoneNumber}`
